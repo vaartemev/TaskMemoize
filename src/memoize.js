d76fc09fb8869a0b0f1430d84b6478c84a213334
Vladimir Artemev <Vladimir_Artemev@epam.com>
2019-07-15 12:20:01 +0300

    Add solution to memoize.js

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,21 @@
-export function memoize() {
-//    TODO
+export function memoize(func) {
+    if (typeof func === 'undefined' || typeof func !== 'function') {
+        return null;
+    }
+
+    const cache = {};
+
+    function memoized(...args) {
+        const key = JSON.stringify(args);
+
+        if (key in cache) {
+            return cache[key];
+        }
+
+        const result = func.apply(this, args);
+        cache[key] = result;
+        return result;
+    }
+
+    return memoized;
 }
